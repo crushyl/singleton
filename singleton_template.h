e47ca5f4712c42500c103f78a54e4c1b31a8de91
Guangkai Cheng <chengguangkai@tsari.tsinghua.edu.cn>
2023-11-17 18:47:55 +0800

    feat: class singleton template

---
+++ singleton_template.h
@@ -0,0 +1,45 @@
+#pragma once
+#include <atomic>
+
+template<typename T>
+class SingletonTemplate {
+ public:
+  struct Init {
+    Init() {
+      auto& count = refCount();
+      auto ori = count.fetch_add(1);
+      if (ori == 0) {
+        T* ptr = SingletonTemplate::Ptr();
+        new (ptr) T();
+      }
+    }
+
+    ~Init() {
+      auto& count = refCount();
+      auto ori = count.fetch_sub(1);
+      if (ori == 1) {
+        T* ptr = SingletonTemplate::Ptr();
+        ptr->~T();
+      }
+    }
+
+    static auto& refCount() {
+      static std::atomic<unsigned> count{0};
+      return count;
+    }
+
+    Init(const Init&) = delete;
+    Init& opreator= (const Init&) = delete;
+  };
+ protected:
+  SingletonTemplate() = default;
+  ~SingletonTemplate() = default;
+  SingletonTemplate(const SingletonTemplate&) = delete;
+  SingletonTemplate& operator= (const SingletonTemplate&) = delete;
+
+ public:
+  static T* Ptr() {
+    alignas(T) static char sing_buf(sizeof(T));
+    return reinterpret_cast<T*>(sing_buf);
+  }
+};
